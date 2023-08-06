ef39948576ecf5c9a6e43fde9cb463367516a757
thuan-NQT <92578416+SwowS-bot@users.noreply.github.com>
2023-08-06 17:40:12 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Button } from "../components/ui/button";
-import Quiz from "./quiz/page";
+import Quizpage from "./quiz/page";
 
 export default function Home() {
   return <Button>hoho</Button>;
