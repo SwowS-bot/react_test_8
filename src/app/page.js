799bf295497e75daea38da7f237a3fa686bbab58
thuan-NQT <92578416+SwowS-bot@users.noreply.github.com>
2023-08-06 12:38:49 +0700

    change local

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,7 @@
 import Image from "next/image";
-import { Button } from "../../@/components/ui/button";
-import Quiz from "./quiz/page"
+import { Button } from "../components/ui/button";
+import Quiz from "./quiz/page";
 
 export default function Home() {
-  return (
-    <main>
-      <Button>nonono</Button>
-      <div>
-        <Quiz/>
-      </div>
-    </main>
-  );
+  return <Button>hoho</Button>;
 }
